4c6a404fd24703d80bea2bae2388f246cb3904ae
Thijmen Jong <thijmenjong2712@hotmail.nl>
2023-02-27 13:38:53 +0100

    Remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@
  */
 
 import { NgModule } from '@angular/core';
-import { FlexModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
