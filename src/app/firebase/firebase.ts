0daff3db2401b7c5b5128e35c0d044e0ba8389b7
Thijmen Jong <thijmenjong2712@hotmail.nl>
2023-02-27 13:37:29 +0100

    Added firebase

---
+++ src/app/firebase/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDZHN-DgCFc9DzfJbB-wY71jlJbzksODHE',
+  authDomain: 'bijroel-70678.firebaseapp.com',
+  projectId: 'bijroel-70678',
+  storageBucket: 'bijroel-70678.appspot.com',
+  messagingSenderId: '577898507037',
+  appId: '1:577898507037:web:a78d147b0057f049af8ff4',
+  measurementId: 'G-TWG3LLNR2B',
+};
+
+const app = initializeApp(firebaseConfig);
